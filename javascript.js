4e2646d11de427db192f918d29ec77fa7a5d52e8
dahmed <DhulkiflA@Gmail.com>
2022-11-27 13:22:29 +1300

    Add console.log statement to js to view how choice is changing

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
+    console.log(choice)
+
     if (choice == 3) {
         return "Paper";
     } else if (choice == 2) {
@@ -10,5 +12,3 @@ function getComputerChoice() {
         return "scissors"
     }
 }
-
-console.log(getComputerChoice)
