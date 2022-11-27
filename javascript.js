94cb52227ece1f9aec5947902d072a6936d3943e
dahmed <DhulkiflA@Gmail.com>
2022-11-27 14:03:42 +1300

    Add if statements to determine whether computer selects Rock, Paper or Scissors

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 function getComputerChoice(){
-    return Math.floor((Math.random()) + 1);
+    let num = Math.floor((Math.random() * 3) + 1);
+    
+    if (num == 1) {
+        return "Rock"
+    } else if (num == 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
 
-console.log(getComputerChoice())
