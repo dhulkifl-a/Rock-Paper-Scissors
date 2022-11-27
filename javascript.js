6bf22cfeeb686210648e73bf6a04cca86f17209f
dahmed <DhulkiflA@Gmail.com>
2022-11-27 13:32:22 +1300

    Delete code and test if the issueis with the external js link

--- javascript.js
+++ javascript.js
@@ -1,14 +1,1 @@
-
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-
-    console.log(choice)
-
-    if (choice == 3) {
-        return "Paper";
-    } else if (choice == 2) {
-        return "Rock"
-    } else {
-        return "scissors"
-    }
-}
+console.log("Hello World")
