b0d5b77cfce42a58817c3f308702d1e362d9077d
dahmed <DhulkiflA@Gmail.com>
2022-12-02 15:55:36 +1300

    Add computer winning option to the code

--- javascript.js
+++ javascript.js
@@ -12,15 +12,19 @@ function getComputerChoice(){
 
 function playRound(playerSelection){
     const computerSelection = getComputerChoice();
-
-    if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return "You Win! Rock beats Scissors";
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return "You Win! Scissors beats Paper";
-    } else if (playerSelection == "Paper" && computerSelection == "Rock"){
-        return "You Win! Rock beats Scissors"
-    } else {
-        return "You lose! Try again!"
+    const playerSelectionCapital = playerSelection.CharAt(0).toUpperCase() + playerSelection.slice(1)
+    if (playerSelectionCapital == "Rock" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors!";
+    } else if (playerSelectionCapital == "Scissors" && computerSelection == "Paper") {
+        return "You Win! Scissors beats Paper!";
+    } else if (playerSelectionCapital == "Paper" && computerSelection == "Rock"){
+        return "You Win! Rock beats Scissors!"
+    } else if (playerSelectionCapital == "Rock" && computerSelection == "Paper"){
+        return "You lose! Paper beats Rock!"
+    } else if (playerSelectionCapital == "Scissors" && computerSelection == "Rock"){
+        return "You lose! Rock beats Scissors!"
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        return "You lose! Scissors beats Paper!"
     }
 }
 
