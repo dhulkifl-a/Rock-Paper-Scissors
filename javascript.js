e6558a977b24b04acd6b1822e5de63012c571e39
dahmed <DhulkiflA@Gmail.com>
2023-09-16 16:51:16 +1200

    Add final details

--- javascript.js
+++ javascript.js
@@ -8,15 +8,24 @@ let playerValue = document.getElementById("playerScore").innerHTML;
 let compValue = document.getElementById('compScore').innerHTML;
 let playerScore = parseInt(playerValue);
 let compScore = parseInt(compValue);
-let RockImg = document.createElement('img');
-let PaperImg = document.createElement('img');
-let ScissorsImg = document.createElement('img');
-RockImg.src = "./images/Rock.png";
-RockImg.setAttribute("width", "82");
-PaperImg.src = "images/paper2.png";
-PaperImg.setAttribute("width", "82");
-ScissorsImg.src = "images/Scissors.png";
-ScissorsImg.setAttribute("width", "82");
+let RockImg_player = document.createElement('img');
+let PaperImg_player = document.createElement('img');
+let ScissorsImg_player = document.createElement('img');
+let RockImg_computer = document.createElement('img');
+let PaperImg_computer = document.createElement('img');
+let ScissorsImg_computer = document.createElement('img');
+RockImg_player.src = "./images/Rock.png";
+RockImg_player.setAttribute("width", "82");
+PaperImg_player.src = "images/paper2.png";
+PaperImg_player.setAttribute("width", "82");
+ScissorsImg_player.src = "images/Scissors.png";
+ScissorsImg_player.setAttribute("width", "82");
+RockImg_computer.src = "./images/Rock.png";
+RockImg_computer.setAttribute("width", "82");
+PaperImg_computer.src = "images/paper2.png";
+PaperImg_computer.setAttribute("width", "82");
+ScissorsImg_computer.src = "images/Scissors.png";
+ScissorsImg_computer.setAttribute("width", "82");
 
 
 
@@ -48,6 +57,7 @@ function handleClick(playerSelection){
 
 
     //Update 
+    UpdateTitle(playerSelection, computerSelection);
     UpdateImg(playerSelection, computerSelection);
     UpdateScore(playerScore, compScore);
     isGameOver(playerScore, compScore);
@@ -94,9 +104,11 @@ function closePopup() {
     compScore = 0;
     document.getElementById('playerScore').innerHTML = playerScore;
     document.getElementById('compScore').innerHTML = compScore;
-    document.getElementById('end-game-message').innerHTML = ""
-    document.getElementById('preview-player').innerHTML = "?"
-    document.getElementById('preview-computer').innerHTML = "?"
+    document.getElementById('end-game-message').innerHTML = "";
+    document.getElementById('preview-player').innerHTML = "?";
+    document.getElementById('preview-computer').innerHTML = "?";
+    document.getElementById("main-title").innerHTML = "Choose your weapon";
+    document.getElementById("secondary-title").innerHTML = "First to score 5 points wins the game";
 }
 
 //Check if Game is over
@@ -115,24 +127,55 @@ function isGameOver(playerScore, compScore) {
 function UpdateImg(playerSelection, computerSelection) {
     if (playerSelection == "Rock") {
         document.getElementById("preview-player").innerHTML = ""
-        document.getElementById("preview-player").appendChild(RockImg);
+        document.getElementById("preview-player").appendChild(RockImg_player);
     } else if (playerSelection == "Scissors") {
         document.getElementById("preview-player").innerHTML = ""
-        document.getElementById('preview-player').appendChild(ScissorsImg);
+        document.getElementById('preview-player').appendChild(ScissorsImg_player);
     } else if (playerSelection == "Paper") {
         document.getElementById("preview-player").innerHTML = ""
-        document.getElementById('preview-player').appendChild(PaperImg);
+        document.getElementById('preview-player').appendChild(PaperImg_player);
     } 
     
     if (computerSelection == "Rock") {
         document.getElementById('preview-computer').innerHTML = ""
-        document.getElementById('preview-computer').appendChild(RockImg);
+        document.getElementById('preview-computer').appendChild(RockImg_computer);
     } else if (computerSelection == "Scissors") {
         document.getElementById('preview-computer').innerHTML = ""
-        document.getElementById('preview-computer').appendChild(ScissorsImg);
+        document.getElementById('preview-computer').appendChild(ScissorsImg_computer);
     } else if (computerSelection == "Paper") {
         document.getElementById('preview-computer').innerHTML = ""
-        document.getElementById('preview-computer').appendChild(PaperImg);
+        document.getElementById('preview-computer').appendChild(PaperImg_computer);
     }
 }
 
+function UpdateTitle(playerSelection, computerSelection) {
+
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        document.getElementById("main-title").innerHTML = "You Won!";
+        document.getElementById("secondary-title").innerHTML = "Rock beats scissors";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        document.getElementById("main-title").innerHTML = "You Won!";
+        document.getElementById("secondary-title").innerHTML = "Scissors beats paper";
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        document.getElementById("main-title").innerHTML = "You Won!";
+        document.getElementById("secondary-title").innerHTML = "Paper beats rock";
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        document.getElementById("main-title").innerHTML = "You Lost!";
+        document.getElementById("secondary-title").innerHTML = "Paper beats Rock";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        document.getElementById("main-title").innerHTML = "You Lost!";
+        document.getElementById("secondary-title").innerHTML = "Rock beats scissors";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        document.getElementById("main-title").innerHTML = "You Lost!";
+        document.getElementById("secondary-title").innerHTML = "Scissors beats paper";
+    } else if (playerSelection == "Paper" && computerSelection == "Paper") {
+        document.getElementById("main-title").innerHTML = "Draw!";
+        document.getElementById("secondary-title").innerHTML = "You both picked paper";
+    } else if (playerSelection == "Rock" && computerSelection == "Rock") {
+        document.getElementById("main-title").innerHTML = "Draw!";
+        document.getElementById("secondary-title").innerHTML = "you both picked rock";
+    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        document.getElementById("main-title").innerHTML = "Draw!";
+        document.getElementById("secondary-title").innerHTML = "you both picked scissors";
+    }
+}
